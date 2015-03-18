89ee35360b2aafe6e57fe3e66d71cb81e6ba6148
Paul Batchelor <pbatchelor@berklee.edu>
2015-03-17 21:48:40 -0400

    updated seq

--- e/seq.c
+++ e/seq.c
@@ -1,24 +1,38 @@
 #include <soundpipe.h>
 #include "../lsamp.h"
 
+#define NUMCHAN 4
+#define STEPS 16
+#define SPEED 5000
+
+#define DOSEQ(NAME,CHAN) if(ud->NAME[ud->step] != -1) { \
+    ud->chan[CHAN].reg = ud->NAME[ud->step]; \
+    ud->chan[CHAN].pos = 0; \
+    lsamp_trig(&ud->chan[CHAN]); \
+}
+
 typedef struct {
     uint32_t reg;
     uint32_t pos;
-    uint32_t bufpos;
+    int32_t bufpos;
     lsamp_cbuf buf;
 }lsamp_channel;
 
 typedef struct {
     lsamp_data *ld;
-    lsamp_channel chan[4];
+    lsamp_channel chan[NUMCHAN];
     int clock;
+    int step;
+    int *kick;
+    int *snare;
+    int *hihat;
 }user_data; 
 
 void lsamp_init_channel(lsamp_channel *chan) {
     chan->reg = 0;
     chan->pos = 0;
-    /* set to negative one for initalization */
-    chan->bufpos = -1;
+    /* set to negative two for initialization */
+    chan->bufpos = -2;
     lsamp_cbuf_init(&chan->buf);
 }
 
@@ -34,12 +48,12 @@ void load_buffer(lsamp_data *ld, lsamp_channel *chan){
 }
 
 /* this will force a reload of the internal buffer when lsamp_get is called 
- * again on the particular channel 
+ * on the particular channel 
  *
- * This is needed for changing between samples on a single channel.
+ * You will need this to trigger and LSAMP instrument at all.
  *
  */
-void lsamp_retrig(lsamp_channel *chan) {
+void lsamp_trig(lsamp_channel *chan) {
         chan->bufpos = -1;
 }
 
@@ -49,21 +63,30 @@ void lsamp_retrig(lsamp_channel *chan) {
  * This function will pop the next value in the circular buffer and return
  * it into the LSAMP_FLOAT pointer out.
  *
- * When the buffer position is full or initalized (-1), it will
+ * When the buffer position is full at -1, it will
  * read from the lsamp file and fill the circular buffer. 
+ *
+ * By default, the buffer position is set to -2, which will return 0;
  */
 void lsamp_get(lsamp_data *ld, lsamp_channel *chan, LSAMP_FLOAT *out){
-
-    if(chan->bufpos >= LSAMP_BUFFER_SIZE || chan->bufpos == -1) {
-        load_buffer(ld, chan);
+    if(chan->bufpos != -2){
+        if(chan->bufpos >= LSAMP_BUFFER_SIZE || chan->bufpos == -1) {
+            load_buffer(ld, chan);
+        }
+        lsamp_cbuf_get_val(&chan->buf, out); 
+        chan->bufpos++;
+    }else{
+        *out = 0;
     }
-    lsamp_cbuf_get_val(&chan->buf, out); 
-    chan->bufpos++;
 }
 
 void init(user_data *ud) {
     ud->clock = 0;
-    lsamp_init_channel(&ud->chan[0]);
+    ud->step = -1;
+    int i;
+    for(i = 0; i < NUMCHAN; i++) { 
+        lsamp_init_channel(&ud->chan[i]);
+    }
     lsamp_create_header(&ud->ld);
 }
 
@@ -72,27 +95,52 @@ void destroy(user_data *ud) {
 }
 
 void process(sp_data *sp, void *userdata) {
+    int i;
     user_data *ud = (user_data *)userdata;
-    if(ud->clock == 5000){
-        ud->chan[0].pos = 0;
-        ud->clock = 0;
-        if(ud->chan[0].reg == 0){
-            ud->chan[0].reg = 1;
-        }else{
-            ud->chan[0].reg = 0;
-        }
-        lsamp_retrig(&ud->chan[0]);
+    LSAMP_FLOAT tmp, sum;
+    sum = 0;
+    if(ud->clock == 0){
+        ud->step = (ud->step + 1) % 16;
+        DOSEQ(kick, 0);
+        DOSEQ(snare, 1);
+        DOSEQ(hihat, 2);
     }
-    ud->clock++;
-    lsamp_get(ud->ld, &ud->chan[0], &sp->out);
+    for(i = 0; i < NUMCHAN; i++){
+        lsamp_get(ud->ld, &ud->chan[i], &tmp);
+        sum += tmp;
+    }
+
+    sp->out = sum;
+    ud->clock = (ud->clock + 1) % 5000;
 }
 
 int main() {
     sp_data *sp;
     user_data ud;
-    init(&ud);
-    sp_create(&sp);
+    int k[] = {
+        0, -1, -1, -1, 
+        0, -1, -1, -1, 
+        0, -1, -1, -1, 
+        0, -1, -1, -1,
+    };
+    int s[] = {
+        -1, -1, -1, -1, 
+        1, -1, 2, -1, 
+        -1, -1, -1, -1, 
+        1, -1, -1, 2,
+    };
+    int h[] = {
+        -1, -1, 6, -1, 
+        -1, -1, 6, -1, 
+        -1, -1, 6, -1, 
+        -1, -1, 6, -1,
+    };
 
+    init(&ud);
+    ud.kick = k;
+    ud.snare = s;
+    ud.hihat= h;
+    sp_create(&sp);
     lsamp_read_header(ud.ld, "../smp/tr909.smp");
     sp_process(sp, &ud, process);
 
